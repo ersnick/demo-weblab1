afd3b3d1204ceff62706d17e4be6f40b49cb547b
Nikita Ershov <137996541+ersnick@users.noreply.github.com>
2023-10-14 16:14:44 +0500

    Initial commit

---
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -0,0 +1,28 @@
+package com.example.demoweb.service;
+
+import com.example.demoweb.model.Post;
+import com.example.demoweb.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+
+@Service
+public class PostService {
+    @Autowired
+    PostRepository postRepository;
+
+    public Iterable<Post> listAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public void create(String text) {
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
+    }
+
+}
---
+++ src/main/resources/static/main.js
@@ -0,0 +1,13 @@
+console.log(fghjkl)
+window.onload = function() {
+    $(".like").click((e) => {
+       var btn = $(e.currentTarget);
+       var postId = btn.attr("data-post-id");
+       btn.attr("disabled", "disabled");
+       $.post("/post/" + postId + "/like", (data) => {
+          btn.text("" + data + " ❤️");
+          btn.removeClass("btn-secondary");
+          btn.addClass("btn-danger");
+       });
+    });
+});
